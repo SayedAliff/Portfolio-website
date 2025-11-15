d62c6cdcf0b33b92e95b93a39c99850af700b69f
Sayed Nafisur Rahman Alif <143423207+SayedAliff@users.noreply.github.com>
2025-11-15 17:53:06 +0600

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   // Assuming the metadata object is configured here
-  title: "Devfolio | Portfolio Website",
+  title: "Sayed Nafisur Rahman Alif",
   description: "A developer portfolio built with Next.js.",
 };
 
